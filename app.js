21a90a5fb983bc30d56029e826d0bbc21fd40092
srujana-malyala <malyalasrujana40@gmail.com>
2025-11-23 22:03:42 +0530

    add

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// 1. TOGGLE BUTTON
 const toggleBtn = document.getElementById("toggleBtn");
 const closeBtn = document.getElementById("closeBtn");
 const sidebar = document.getElementById("sidebar");
@@ -20,3 +21,36 @@ const CATEGORIES_API = "https://www.themealdb.com/api/json/v1/1/categories.php";
 const SEARCH_API = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 const FILTER_API = "https://www.themealdb.com/api/json/v1/1/filter.php?c=";
 const DETAILS_API = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
+
+
+// 3. LOAD CATEGORIES ON HOMEPAGE
+
+const loadCategories = async () => {
+    const box = document.getElementById("categoryList");
+    const sideList = document.getElementById("sideList");
+
+    if (!box) return;
+
+    const res = await fetch(CATEGORIES_API);
+    const data = await res.json();
+    const categories = data.categories;
+
+    categories.forEach(category => {
+        box.innerHTML += `
+            <div class="card" onclick="openCategory('${category.strCategory}')">
+                <img src="${category.strCategoryThumb}">
+                <p>${category.strCategory}</p>
+            </div>
+        `;
+
+        if (sideList) {
+            sideList.innerHTML += `
+                <li onclick="openCategory('${category.strCategory}')">
+                    ${category.strCategory}
+                </li>
+            `;
+        }
+    });
+};
+
+loadCategories();
